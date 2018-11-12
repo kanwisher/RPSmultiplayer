7ced00a03520dbbaf5f7ad4455d5e76fd5acf2a3
kanwisher <kanwisher@gmail.com>
2018-11-11 21:16:38 -0500

    add player logic, still stuck on gameplay behavior

--- main.js
+++ main.js
@@ -9,12 +9,113 @@ document.addEventListener("DOMContentLoaded", () => {
     messagingSenderId: "635289483910"
   };
   firebase.initializeApp(config);
+
+
   const db = firebase.database();
-  const root = db.ref();
-  const testObj = {
-    name: "David",
-    age: 31
+  const rootRef = db.ref();
+  const playersRef = rootRef.child("/players");
+  // const game = rootRef.child("/game");
+
+  let playerCount = 0;
+  let hasPlayerOne = false;
+  let localPlayerRef;
+  let localPlayerNum;
+
+
+
+  playersRef.on("value", snap => {
+    playerCount = snap.numChildren();
+    hasPlayerOne = snap.child("1").exists();
+    hasPlayerTwo = snap.child("2").exists();
+
+    if (playerCount < 2 && !localPlayerNum) { //game not full and local player unassigned
+      if (!hasPlayerOne) {
+        createPlayer(1);
+      } else {
+        createPlayer(2); //player one already exists, create local as player 2
+      } 
+    } else {
+      playGame();
+    }
+  });
+
+  function playGame() {
+    if (localPlayerNum) { // local computer is playing
+      
+    } else {
+      alert("Please wait for spot to become available");
+    }
   }
-  root.set(testObj);
+
+
+
+  function createPlayer(num) {
+    localPlayerRef = playersRef.child(num);
+    localPlayerNum = num; // keep reference number of local player, use as flag for local player existing
+    localPlayerRef.onDisconnect().remove(); // if local closes browser, delete player
+    const nickName = prompt(`What's your nickname player ${num}?`)
+    const localPlayerInfo = {
+      name: nickName,
+      pick: null,
+      wins: 0,
+      losses: 0,
+      ties: 0
+    };
+    localPlayerRef.set(localPlayerInfo)
+  }
+
+
+
+
+
+  //failed code
+  // function playGame() {
+  //   if (localPlayerNum) { // if current user is actually a player
+  //     console.log("game on");
+  //   } else {
+  //     alert("game in progress, wait in line punk");
+  //   }
+  // }
+  // const localPlayerInfo = {
+  //   name: nickName,
+  //   pick: null,
+  //   wins: 0,
+  //   losses: 0,
+  //   ties: 0
+  // };
+
+ 
+  // playerOneRef.on("value", snap => {
+  //   if (!snap.val() && !inGamePlayer) {
+  //     playerOneRef.set(localPlayerInfo);
+  //     inGamePlayer = playerOneRef
+  //     handleDisconnect(playerOneRef); // set this once client player is known
+  //     rootRef.on("value", snap => {
+  //       if (snap.numChildren() === 2) {
+  //         alert("game on!");
+  //       }
+  //     });
+      
+  //   }
+  // });
+
+  // playerTwoRef.on("value", snap => {
+  //   if (!snap.val() && !inGamePlayer) {
+  //     playerTwoRef.set(localPlayerInfo);
+  //     inGamePlayer = playerTwoRef
+  //     handleDisconnect(playerTwoRef);
+  //     rootRef.once("value", snap => {
+  //       if (snap.numChildren() === 2) {
+  //         alert("game on!");
+  //       }
+  //     });
+  //   }
+  // });
+
+  
+
+  // function handleDisconnect(ref) { //once client disconnects, remove them if they were player one or two
+  //   ref.onDisconnect().remove(); //whenb I close my browser, remove myself from list
+  // }
 
 });
